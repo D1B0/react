adf6b722fff35e4b50a31d9dd7686829db579a48
D1B0 <79042710+D1B0@users.noreply.github.com>
2021-07-27 19:15:01 +0300

    Add files via upload
    Fix component Message
    add styles to component Message
    change imports path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {Message} from './components/Message'
-import styles from "./styles/app.module.css";
+import styles from "./app.module.css";
 
 function App() {
     const message = {name: 'good hacker'}
     return (
         <div className={styles.app} style={{ border: "1px solid red" }}>
-            <Message message={message}/>
+            <Message message={message.name}/>
         </div>
     );
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,5 @@
-import styles from "../styles/app.module.css";
+import styles from "./message.module.css";
 
 export function Message(props) {
-    console.log(props)
-    return <h1 className={styles.message}>Привет, {props.message.name}</h1>;
+    return <h1 className={styles.message}>Привет, {props.message}</h1>;
 }
